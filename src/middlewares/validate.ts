b04970a94d8119769dbe00e9e8558e0d61fe03ef
Naman Dubey <namandubey10@gmail.com>
2025-12-10 20:26:19 +0530

    Added middleware for validating schema

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import z from "zod";
+import { ZodError } from "../core/CustomError";
+
+export const validate = (schema: z.ZodType) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const result = schema.safeParse(req.body);
+
+        if(!result.success){
+            throw new ZodError(`Error while parsing schema ${schema}`);
+        }
+
+        req.body = result.data;
+        next();
+    }
+};
