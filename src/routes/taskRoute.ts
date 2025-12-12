6bea237f8320547045d003c3c2ca8cdc5443ab10
Naman Dubey <namandubey10@gmail.com>
2025-12-12 14:55:01 +0530

    Updated routes with validation middleware

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { createTaskHandler, getTasksHandler, getTaskByIdHandler } from '../controllers/taskController';
 import asyncHandler from 'express-async-handler';
+import { validate } from '../middlewares/validate';
+import { taskSchema } from '../types/task.schema';
 
 export const taskRouter = express.Router();
 
 taskRouter.get('/:id', asyncHandler(getTaskByIdHandler));
-taskRouter.post('/create', asyncHandler(createTaskHandler));
+taskRouter.post('/create', validate(taskSchema), asyncHandler(createTaskHandler));
 taskRouter.get('/', asyncHandler(getTasksHandler));
 
 
