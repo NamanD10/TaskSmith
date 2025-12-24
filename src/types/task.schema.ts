d1aef9e0cd5cb7bd7e5f175abdba6b273adcf974
Naman Dubey <namandubey10@gmail.com>
2025-12-24 16:39:38 +0530

    Added the update schema and chagned type of scheduledAt from string to date

--- src/types/task.schema.ts
+++ src/types/task.schema.ts
@@ -4,11 +4,10 @@ export const taskSchema = z.object({
     title: z.string().min(1),
     description: z.string().min(2),
     type: z.string().min(2),
-    scheduledAt: z.string().optional(),
+    scheduledAt: z.date().optional(),
     isRepeatable: z.boolean(),
     priority: z.number(),
     repeatPattern: z.string().optional(),
-
 });
-
+export const taskUpdateSchema = taskSchema.partial();
 export type Task = z.infer<typeof taskSchema>;
