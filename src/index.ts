ac8cbdc94b6405ce4b0d73d637f5328be55b4b08
Naman Dubey <namandubey10@gmail.com>
2026-01-12 20:57:41 +0530

    Added code to run all the workers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { emailWorker } from "./workers/taskWorker";
 import app from "./app";
 import dotenv from 'dotenv';
+import { primaryWorker } from "./workers/taskWorker";
+import { secondWorker } from "./workers/secondWorker";
+import { thirdWorker } from "./workers/thirdWorker";
+import { fourthWorker } from "./workers/fourthWorker";
 
 dotenv.config();
-emailWorker.run();
+
+primaryWorker.run();
+secondWorker.run();
+thirdWorker.run();
+fourthWorker.run();
     
 const port = process.env.PORT;
     
