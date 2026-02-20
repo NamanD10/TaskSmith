82f330034b02ae8a399fc41a7b9719ccff21d9b3
Naman Dubey <namandubey10@gmail.com>
2026-02-20 14:47:57 +0530

    Setup for the test environment

--- src/test/jest.setup.ts
+++ src/test/jest.setup.ts
@@ -1,2 +1,2 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "D:/Coding/Programs/Backend/TaskSmith/.env" }); // or just ".env" if using the same
+dotenv.config({ path: "D:/Coding/Programs/Backend/TaskSmith/.env" }); 
