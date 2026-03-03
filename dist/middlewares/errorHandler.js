62c079a5a101b7bfaabd3cfbcc66875400adf515
Naman Dubey <namandubey10@gmail.com>
2026-03-03 20:50:35 +0530

    Added /dist to gitignore

--- dist/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(error, req, res, next) {
-    return res.status(error.statusCode).json({
-        message: error.message,
-    });
-}
