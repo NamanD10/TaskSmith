09c6456355b50ae3ac2d55d45a4e66d8b7ba7eab
Naman Dubey <namandubey10@gmail.com>
2026-09-07 21:27:00 +0530

    Added better-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from '../db/schema';
+import { db } from "../db/db";
+
+export const auth = betterAuth({
+    
+    trustedOrigins: ["http://localhost:5143", "http://localhost:8008", "http://localhost:3000"],
+
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema
+    }),
+
+    emailAndPassword: { 
+        enabled: true, 
+    }, 
+    socialProviders: { 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID! as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET! as string, 
+        }, 
+    },
+});
