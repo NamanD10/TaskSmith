d8f50f44d641e36fdd0d51dec4eb9f949ca0daea
Naman Dubey <namandubey10@gmail.com>
2026-09-07 21:29:38 +0530

    Auth working

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,42 @@ import { taskRouter } from './routes/taskRoute';
 import { ApiError } from './core/AppError';
 import { InternalError } from './core/CustomError';
 import { queueRouter } from './routes/queueRoute';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
+import dotenv from 'dotenv';
+import { requireAuth } from './middlewares/auth';
+// import { primaryWorker } from "./workers/taskWorker";
+// import { secondWorker } from "./workers/secondWorker";
+// import { thirdWorker } from "./workers/thirdWorker";
+// import { fourthWorker } from "./workers/fourthWorker";
+
+dotenv.config();
+
+// primaryWorker.run();
+// secondWorker.run();
+// thirdWorker.run();
+// fourthWorker.run();
 
 const app = express();
+const port = process.env.PORT;
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.use(express.json());
 
-app.use('/tasks', taskRouter);
-app.use('/queue', queueRouter);
+app.use('/tasks', requireAuth, taskRouter);
+app.use('/queue', requireAuth, queueRouter);
 
 app.use((err: Error, req : Request, res: Response, next: NextFunction) => {
     if(err instanceof ApiError) {
         ApiError.handle(err, res)
     }
     ApiError.handle(new InternalError(), res);
-})
-export default app;
+});
 
+
+app
+ .listen(port, () => {
+     console.log(`App listening on port ${port}`)
+ })
+ .on("error", e => console.error(e));
