3a59b2c192f5c77981fabc309c99b76b71e9c7c5
Naman Dubey <namandubey10@gmail.com>
2026-09-09 20:57:10 +0530

    Updated functions according to new schema

--- src/models/taskModel.ts
+++ src/models/taskModel.ts
@@ -1,41 +1,46 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { taskTable } from '../db/schema';
-import { InferInsertModel } from 'drizzle-orm';
+import { db } from '../db/db';
+import { task } from '../db/schema';
+import { and, InferInsertModel } from 'drizzle-orm';
 import { eq } from 'drizzle-orm';
-import dotenv from "dotenv";
 import { InternalError, NotFoundError } from '../core/CustomError';
+import dotenv from "dotenv";
+import { HeadersType, ReqBodyType } from '../types/task.schema';
 dotenv.config();
 
-const db = drizzle(process.env.DATABASE_URL!);
+
+type TaskUpdate = Partial<InferInsertModel<typeof task>>;
+type ReqMethodType =  "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | null | undefined
 
 
-type TaskUpdate = Partial<InferInsertModel<typeof taskTable>>;
-
-
-export const createdTask = async (title: string, description: string, type: string, isRepeatable: boolean, scheduledAt: Date | null, repeatPattern: string | null, priority: number ) =>{ 
-    const result =  await db.insert(taskTable)
+export const createdTask = async (userId: string | undefined, title: string, targetUrl: string, isRepeatable: boolean, scheduledAt: Date | null, repeatPattern: string | null, priority: number, reqMethod: ReqMethodType,  headers: HeadersType, reqBody: ReqBodyType) =>{ 
+    const result =  await db.insert(task)
         .values({ 
+            userId: userId,
             title: title,
-            description: description,
-            type: type,
+            targetUrl : targetUrl,
             isRepeatable: isRepeatable,
             scheduledAt: scheduledAt,
             repeatPattern: repeatPattern,
             priority : priority,
+            reqMethod : reqMethod,
+            headers : headers,
+            reqBody : reqBody
         }
         ).returning();  
     return result[0];
 };
 
-export const updateTask = async (id: number, data: TaskUpdate) => {
+export const updateTask = async (userId: string, id: string, data: TaskUpdate) => {
 
-    const result = await db.update(taskTable)
+    const result = await db.update(task)
         .set({
-            ...data,
-            updatedAt: new Date(),
+            ...data
         })
         .where(
-            eq(taskTable.id, id)
+            and(
+                eq(task.userId, userId),
+                eq(task.id, id)
+            )    
         )
         .returning();
 
@@ -46,22 +51,35 @@ export const updateTask = async (id: number, data: TaskUpdate) => {
     
 };
 
-export const getTaskById = async (id: number) => {
+export const getTaskById = async (userId:string, id: string) => {
     const result = await db.
     select()
-    .from(taskTable)
-    .where(eq(taskTable.id, id)).limit(1);
+    .from(task)
+    .where(
+        and(
+            eq(task.userId, userId),
+            eq(task.id , id)
+        )
+    )
+    
     
     return result[0];
 };
 
-export const getTasks = async () => {
-    return await db.select().from(taskTable);
+export const getTasks = async (userId: string) => {
+    return await db.select()
+    .from(task)
+    .where(eq(task.userId, userId));
 };
 
-export const deleteTask = async (id : number) => {
-    const result = await db.delete(taskTable)
-    .where(eq(taskTable.id, id))
+export const deleteTask = async (userId: string, id: string) => {
+    const result = await db.delete(task)
+    .where(
+        and(
+            eq(task.userId, userId),
+            eq(task.id , id)
+        )
+    )
     .returning()
 
     if(!result[0]){
