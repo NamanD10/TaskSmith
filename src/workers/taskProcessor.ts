8edc20b101ef384ccfdbc302bc29805bd8d36580
Naman Dubey <namandubey10@gmail.com>
2026-09-11 21:05:18 +0530

    Edited according to the new functionalities

--- src/workers/taskProcessor.ts
+++ src/workers/taskProcessor.ts
@@ -1,37 +1,38 @@
-import { InternalError } from "../core/CustomError";
+import { BadRequestError, InternalError } from "../core/CustomError";
 import { getTaskById, updateTask } from "../models/taskModel";
-import { makeApiCall, performDatabaseOperation, performDefaultWork, performFileOperation } from "./processFunctions";
+import { makeApiCall } from "./processFunctions";
+import parser from 'cron-parser';
 
-export default async function processTask(taskId: number) {
-    const task = await getTaskById(taskId);
+export default async function processTask(userId: string, taskId: string) {
+    const task = await getTaskById(userId, taskId);
+    const apiTask = {
+        targetUrl : task.targetUrl,
+        headers : task.headers,
+        reqMethod : task.reqMethod,
+        reqBody : task.reqBody
+    };
 
     const startTime = Date.now();
 
     try {
-        switch(task.type){
-
-            case "api-call" : 
-            await makeApiCall();
-            break;
-
-            case "file-operation" :
-            await performFileOperation(task);
-            break;
-
-            case "database-operations" : 
-            await performDatabaseOperation();
-            break;
-            
-            default: 
-            await performDefaultWork(task);
-            break;
-        }
-
+        await makeApiCall(taskId, apiTask);
+        //cant send apiTask to the function due to missing id field in task.schema (zod)
         const duration = Date.now() - startTime;
         console.log(`[${new Date().toISOString()}] Completed task ${taskId} in ${duration/1000} seconds`);
+        
+        let nextRun = null;
+        if(task.isRepeatable){
+            if(!task.repeatPattern){
+                throw new BadRequestError("Repeat pattern is required for repeatable jobs");
+            }
 
-        await updateTask(taskId, {
+            const interval = parser.parse(task.repeatPattern);
+            nextRun = interval.next().toDate();
+        }
+
+        await updateTask(userId, taskId, {
             status: 'COMPLETED',
+            nextRunAt : nextRun,
         });
     } 
     catch(error : any) {
