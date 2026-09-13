fbed85b982c6fdd037f70652e19e89986ae08ca3
Naman Dubey <namandubey10@gmail.com>
2026-09-13 23:14:18 +0530

    Changed function according to new schema

--- src/models/responseModel.ts
+++ src/models/responseModel.ts
@@ -1,14 +1,25 @@
 import { db } from "../db/db";
 import { response } from "../db/schema";
+import z from 'zod';
 
-export const insertResponse = async (taskId : string, executionDate : Date, attemptNumber : number, statusCode : number, statusMessage : string) => {
-    const result = await db.insert(response)
+const resHeadersSchema = z.record(
+    z.string().min(1).max(200),
+    z.string().max(2000))
+.nullish();
+type ResponseHeadersType = z.infer<typeof resHeadersSchema>;
+
+export const insertResponse = async (taskId : string, executionDate : Date, statusCode : number, statusMessage : string, durationMs : number, responseBody : string | null, resopnseHeaders : ResponseHeadersType, errorCode : string | null, errorMessage : string | null) => {
+    const result = await db.insert(response) 
         .values({
             taskId: taskId,
             executionDate: executionDate,
-            attemptNumber: attemptNumber,
             statusCode: statusCode,
-            statusMessage: statusMessage
+            statusMessage: statusMessage,
+            durationMs : durationMs,
+            responseBody : responseBody ?? null,
+            responseHeaders : resopnseHeaders ?? null,
+            errorCode : errorCode ?? null,
+            errorMessage : errorMessage ?? null
         }
     ).returning();
     return result[0];
