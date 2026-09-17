95d618b0483f1e22512c9344e8b8db29746f6a16
Naman Dubey <namandubey10@gmail.com>
2026-09-17 16:18:20 +0530

    Changes in function to make it adaptable to any number of arguments

--- src/models/responseModel.ts
+++ src/models/responseModel.ts
@@ -8,7 +8,29 @@ const resHeadersSchema = z.record(
 .nullish();
 type ResponseHeadersType = z.infer<typeof resHeadersSchema>;
 
-export const insertResponse = async (taskId : string, executionDate : Date, statusCode : number, statusMessage : string, durationMs : number, responseBody : string | null, resopnseHeaders : ResponseHeadersType, errorCode : string | null, errorMessage : string | null) => {
+type InsertResponseParams = {
+    taskId : string;
+    executionDate : Date;
+    statusCode : number;
+    statusMessage : string;
+    durationMs : number;
+    responseBody?: string | null;
+    responseHeaders?: any | null;
+    errorCode?: string | null;
+    errorMessage?: string | null; 
+};
+
+export const insertResponse = async ({
+    taskId,
+    executionDate, 
+    statusCode, 
+    statusMessage, 
+    durationMs, 
+    responseBody = null, 
+    responseHeaders = null, 
+    errorCode = null, 
+    errorMessage = null   
+} : InsertResponseParams) => {
     const result = await db.insert(response) 
         .values({
             taskId: taskId,
@@ -16,11 +38,11 @@ export const insertResponse = async (taskId : string, executionDate : Date, stat
             statusCode: statusCode,
             statusMessage: statusMessage,
             durationMs : durationMs,
-            responseBody : responseBody ?? null,
-            responseHeaders : resopnseHeaders ?? null,
-            errorCode : errorCode ?? null,
-            errorMessage : errorMessage ?? null
-        }
-    ).returning();
+            responseBody : responseBody,
+            responseHeaders : responseHeaders,
+            errorCode : errorCode,
+            errorMessage : errorMessage
+        })
+        .returning();
     return result[0];
 };
