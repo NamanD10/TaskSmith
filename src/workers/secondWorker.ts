006f2a4889c870d6d7645d1f3431399b40049e9f
Naman Dubey <namandubey10@gmail.com>
2026-09-17 20:55:32 +0530

    Aaoi aaoi aano sora

--- src/workers/secondWorker.ts
+++ src/workers/secondWorker.ts
@@ -4,23 +4,29 @@ import { updateTask } from "../models/taskModel";
 import processTask from "./taskProcessor";
 import { InternalError } from "../core/CustomError";
 
-export const secondWorker = new Worker(
+export const primaryWorker = new Worker(
     'taskQueue', 
     async (job:Job) => {
     
-    const { taskId } = job.data;
+    const { userId, taskId } = job.data;
     
     //update status to processing on 1st attempt
     if(job.attemptsMade === 0){
-        await updateTask(taskId, {status: 'PROCESSING'});;
+        await updateTask(userId, taskId, {
+          status: 'PROCESSING',
+          lastRunAt : new Date()
+        });
         console.log(`Task with id ${taskId}, status changed to 'PROCESSING'`);
     } else {
-        await updateTask(taskId, {status: 'PROCESSING'});
+        await updateTask(userId, taskId, {
+          status: 'PROCESSING',
+          lastRunAt : new Date()
+        });
         console.log(`Retry attempt #${job.attemptsMade} for task ${taskId}`);
     }
 
     try{
-      await processTask(taskId);
+      await processTask(userId, taskId);
     } catch (error : any) {
       throw new InternalError(`Job ${taskId} failed: ${error.message}`);
     }
@@ -31,19 +37,19 @@ export const secondWorker = new Worker(
   }, 
 );
     
-secondWorker.on('completed', async(job, err) => {
+primaryWorker.on('completed', async(job, err) => {
   if(!job) return;
 
-  const { taskId } = job.data;
+  const { userId, taskId } = job.data;
   const attemptsMade = job.attemptsMade + 1;
   
-  await updateTask(taskId, {attempts: attemptsMade});
+  await updateTask(userId, taskId, {attempts: attemptsMade});
 
   console.log(`Task ${job.id} completed by Bull MQ worker`);
     
 });
     
-secondWorker.on('failed', async (job, err) => {
+primaryWorker.on('failed', async (job, err) => {
     
   if (!job) {
     console.error("Job is undefined in 'failed' event");
@@ -52,16 +58,16 @@ secondWorker.on('failed', async (job, err) => {
 
   //?? {} is the null coalescing operator which means
   //if lhs is undefined it (taskId) falls back to {} or empty object
-  const { taskId } = job?.data ?? {};
+  const { userId, taskId } = job?.data ?? {};
   const attemptsMade = job.attemptsMade + 1;  //job.attemptsMade is zero based (like array index)
   
     //update amt of attempts
-  await updateTask(taskId, {attempts: attemptsMade});
+  await updateTask(userId, taskId, {attempts: attemptsMade});
 
   // if no more retries left, mark as permanently failed
   if (job.attemptsMade >= job.opts.attempts!) {   //! at the end is non-null assertion wheer we gurantee typescript that the object/property is not null
     if (taskId) {
-      await updateTask(taskId, {status: 'FAILED'});
+      await updateTask(userId, taskId, {status: 'FAILED'});
       console.error(`Task ${taskId} permanently failed after ${job.attemptsMade} attempts.`);
     }
   } else {
@@ -70,9 +76,6 @@ secondWorker.on('failed', async (job, err) => {
 });
 
 //handling any internal worker error
-secondWorker.on('error', (error : any) =>
+primaryWorker.on('error', (error : any) =>
   {console.error(`Error in primary work`, error.message)}
 ); 
-
-
- 
